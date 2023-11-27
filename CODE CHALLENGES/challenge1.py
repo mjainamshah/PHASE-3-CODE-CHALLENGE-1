354f32eea7af4e2c3596a5f2df99621c1c504116
mjainamshah <jainam.shah@student.moringaschool.com>
2023-11-27 09:53:24 +0300

    CHALLENGE 1

---
+++ CODE CHALLENGES/challenge1.py
@@ -0,0 +1,13 @@
+def convert_to_24_hour(hour, minute, period):
+    if period == "am":               # Checks if the period is in am
+        if hour == 12:               # If it's 12 am, then converts that hour to 0
+            hour = 0
+    else:
+        if hour != 12:               # If period is in pm & not 12 pm, it adds 12 to the hour
+            hour += 12
+    return f"{hour:02d}{minute:02d}" # Return the time in 24-hour format as a four-digit string with leading zeros if necessary
+
+# print(convert_to_24_hour(9, 30, "am"))  
+# print(convert_to_24_hour(9, 30, "pm")) 
+# print(convert_to_24_hour(12, 00, "pm"))   
+# print(convert_to_24_hour(12, 00, "am"))   
---
+++ CODE CHALLENGES/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+    print(convert_to_24_hour(8, 30, "am")) 
